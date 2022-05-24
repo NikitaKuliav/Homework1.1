4131f2ba06808ff1479fd9acf4906defcecb8bfb
Nikita <athlon64x2600@gmail.com>
2022-05-24 17:50:22 +0200

    // Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Первое задание
+
+        byte dogs = 5;
+        short cats = 9;
+        int rubles = 1_000_000;
+        long peoples = 8_000_000_000L;
+
+        float pi = 3.14f;
+        double e = 2.7;
+
+        char a = 97;
+
+        boolean learn = true;
+
     }
 }
